67281aa26767ef02aff4a7697b3c4a1c972fe8c2
Eathi Rajulu Yella <raju.yella@e2open.com>
2023-03-09 14:16:18 +0530

    Initial Checkin

---
+++ src/main/java/com/eyella/multibranchpipelinejenkins/MultibranchPipelineJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.eyella.multibranchpipelinejenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultibranchPipelineJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultibranchPipelineJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eyella/multibranchpipelinejenkins/MultibranchPipelineJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eyella.multibranchpipelinejenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultibranchPipelineJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
